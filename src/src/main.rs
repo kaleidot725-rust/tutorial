088e4afee579a01d343771ee77887a7cc86dc444
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-05-07 22:21:11 +0900

    feat: update

--- src/src/main.rs
+++ src/src/main.rs
@@ -109,7 +109,7 @@ fn main() {
             name: String,
             arms: i32,
             legs: i32,
-            weapon: String
+            weapon: String,
         }
 
         // スタティックメソッド
@@ -117,6 +117,120 @@ fn main() {
 
         // インスタンスメソッド
         println!("{} is {} characters long.", s, s.len());
+
+        let ferris = SeaCreature {
+            animal_type: String::from("crab"),
+            name: String::from("Ferris"),
+            arms: 2,
+            legs: 4,
+            weapon: String::from("claw"),
+        };
+
+        println!(
+            "{} is a {}. They have {} arms, {} legs, and a {} weapon",
+            ferris.animal_type, ferris.name, ferris.arms, ferris.legs, ferris.weapon
+        );
+
+        let sarah = SeaCreature {
+            animal_type: String::from("octopus"),
+            name: String::from("Sarah"),
+            arms: 8,
+            legs: 0,
+            weapon: String::from("none"),
+        };
+
+        println!(
+            "{} is a {}. They have {} arms, {} legs, and a {} weapon",
+            sarah.animal_type, sarah.name, sarah.arms, sarah.legs, sarah.weapon
+        );
+
+        struct Location(i32, i32);
+        let loc = Location(42, 32);
+        println!("{}, {}", loc.0, loc.1);
+
+        struct Marker;
+        let _m = Marker;
+    }
+    println!("-------------");
+
+    // 列挙型
+    println!("No9 ----------");
+    {
+        enum Species {
+            Crab,
+            Octopus,
+            Fish,
+            Clam,
+        }
+
+        struct SeaCreature {
+            species: Species,
+            name: String,
+            arms: i32,
+            legs: i32,
+            weapon: String,
+        }
+
+        let ferris = SeaCreature {
+            species: Species::Crab,
+            name: String::from("Ferris"),
+            arms: 2,
+            legs: 4,
+            weapon: String::from("claw"),
+        };
+
+        match ferris.species {
+            Species::Crab => println!("{} is a crab", ferris.name),
+            Species::Octopus => println!("{} is a octopus", ferris.name),
+            Species::Fish => println!("{} is a fish", ferris.name),
+            Species::Clam => println!("{} is a clam", ferris.name)
+        }
+    }
+    println!("-------------");
+
+    // 列挙型でUnion
+    println!("No10 ----------");
+    {
+        enum Species { Crab, Octopus, Fish, Clam }
+        enum PoisonType { Acidic, Painful, Lethal }
+        enum Size { Big, Small }
+        enum Weapon {
+            Claw(i32, Size),
+            Poison(PoisonType),
+            None,
+        }
+
+        struct SeaCreature {
+            species: Species,
+            name: String,
+            arms: i32,
+            legs: i32,
+            weapon: Weapon,
+        }
+
+        let ferris = SeaCreature {
+            species: Species::Crab,
+            name: String::from("Ferris"),
+            arms: 2,
+            legs: 4,
+            weapon: Weapon::Claw(2, Size::Small),
+        };
+
+        match ferris.species {
+            Species::Crab => {
+                match ferris.weapon {
+                    Weapon::Claw(num_claws, size) => {
+                        let size_description = match size {
+                            Size::Big => "big",
+                            Size::Small => "small"
+                        };
+                        println!("ferris is a crab with {} {} claws", num_claws, size_description)
+                    }
+                    _ => println!("ferris is a  crab with some other weapon")
+                }
+            },
+            _ => println!("ferris is some other animal"),
+        }
     }
     println!("-------------");
 }
